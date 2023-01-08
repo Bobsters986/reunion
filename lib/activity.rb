b0784a82e087b1c92bf752919b3457c8c1777129
Bobby Luly <bobbyluly@gmail.com>
2023-01-07 19:45:34 -0700

    Add answers

--- lib/activity.rb
+++ lib/activity.rb
@@ -7,4 +7,26 @@ class Activity
     @participants = {}
   end
 
+  def add_participant(participant, cost)
+    participants[participant] = cost
+  end
+
+  def total_cost
+    participants.values.sum
+  end
+
+  def split
+    total_cost / participants.count
+  end
+
+  def owed
+    name_money_owed = Hash.new
+    
+    participants.each do |participant, cost|
+      name_money_owed[participant] = (split - cost)
+    end
+
+    name_money_owed
+  end
+
 end
--- spec/activity_spec.rb
+++ spec/activity_spec.rb
@@ -12,7 +12,32 @@ RSpec.describe Activity do
     it 'has attributes' do
       expect(activity.name).to eq("Brunch")
       expect(activity.participants).to eq({})
+    end
 
+    it 'can #add_participants' do
+      activity.add_participant("Maria", 20)
+      expect(activity.participants).to eq({"Maria" => 20})
+
+      activity.add_participant("Luther", 40)
+      expect(activity.participants).to eq({"Maria" => 20, "Luther" => 40})
     end
   end
+
+  describe 'Iteration 2' do
+    it '#split' do
+      activity.add_participant("Maria", 20)
+      activity.add_participant("Luther", 40)
+
+      expect(activity.total_cost).to eq(60)
+      expect(activity.split).to eq(30)
+    end
+
+    it '#owed' do
+      activity.add_participant("Maria", 20)
+      activity.add_participant("Luther", 40)
+
+      expect(activity.owed).to eq({"Maria" => 10, "Luther" => -10})
+    end
+  end
+
 end
